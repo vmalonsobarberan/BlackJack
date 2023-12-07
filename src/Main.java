af07a44443c11a08bc59dbeeca24238e5384b9f4
vmalonsobarberan <vm.alonsobarberan@edu.gva.es>
2023-12-07 20:39:43 +0100

    number of layers and q to quit

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,28 @@ public class Main {
         MultiDeck deck = new MultiDeck(1);
         deck.shuffle();
         Player house = new Player();
-        Player p2 = new Player("Victor", 1000);
-        Player p3 = new Player("Carlos", 1000);
-        List<Player> list = new ArrayList<Player>();
 
-        list.add(p2);
-        list.add(p3);
+        int numPlayers = Global.readValidInteger("Number of players: ");
+        List<Player> players = new ArrayList<Player>();
 
-        Game game = new Game(house, list, deck);
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.print("Enter name of player " + (i+1) + ": ");
+            String name = Global.inputKeyboard.nextLine();
+            int money = Global.readValidInteger("Initial amount of $ for " + name + ": ");
+            Player player = new Player(name, money);
+            players.add(player);
+        }
+
+
+        Game game = new Game(house, players, deck);
         while (true) {
             game.play();
+            System.out.println("Press q to quit");
+            if (Global.inputKeyboard.nextLine().equals("q")) {
+                break;
+            }
         }
+
     }
 
 
